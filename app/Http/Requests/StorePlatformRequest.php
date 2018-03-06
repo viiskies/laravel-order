07376166169df59ee211fcac2f53d6c91cb0ee3c
Shniuras <seimurinho@gmail.com>
2018-03-06 15:00:10 +0200

    Added unique to Platform and Publisher

--- app/Http/Requests/StorePlatformRequest.php
+++ app/Http/Requests/StorePlatformRequest.php
@@ -24,7 +24,7 @@ class StorePlatformRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
+            'name' => 'required|unique:platforms',
         ];
     }
 }
--- app/Http/Requests/StorePublisherRequest.php
+++ app/Http/Requests/StorePublisherRequest.php
@@ -24,7 +24,7 @@ class StorePublisherRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required'
+            'name' => 'required|unique:publishers'
         ];
     }
 }
