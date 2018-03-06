5ba0a2acd42f2ffadf0331a978abb70cdac093e9
Jaroslav Lomecki <jaroslavlo@gmail.com>
2018-03-06 14:27:50 +0200

    Few changes in categories.

--- app/Http/Requests/UpdateCategoryRequest.php
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -1,27 +1,27 @@
 <?php
 	
-	namespace App\Http\Requests;
-	
-	use Illuminate\Foundation\Http\FormRequest;
-	
-	class UpdateCategoryRequest extends FormRequest {
-		/**
-		 * Determine if the user is authorized to make this request.
-		 *
-		 * @return bool
-		 */
-		public function authorize() {
-			return true;
-		}
-		
-		/**
-		 * Get the validation rules that apply to the request.
-		 *
-		 * @return array
-		 */
-		public function rules() {
-			return [
-				'name' => 'required|unique:categories,name,' . $this -> route( 'category' ) . ',id|max:255|min:2'
-			];
-		}
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCategoryRequest extends FormRequest {
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize() {
+		return true;
 	}
+	
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules() {
+		return [
+			'name' => 'required|unique:categories,name,' . $this -> route( 'category' ) . ',id|max:255|min:2'
+		];
+	}
+}
