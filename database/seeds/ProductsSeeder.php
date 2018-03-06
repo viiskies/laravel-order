a00e0a4d77fca73ced73e30345b8f6caf7dc6ca7
Jaroslav Lomecki <jaroslavlo@gmail.com>
2018-03-06 18:32:22 +0200

    Create category seeder

---
+++ database/seeds/ProductsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+use App\Providers;
+use App\Publisher;
+use App\Platform;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//    	$faker = Faker::create();
+//
+//    	for($i = 0; $i < 10; $i ++)
+//    	$platform = Platform::create([
+//    		'name'=>$faker->
+//	    ]);
+    }
+}
