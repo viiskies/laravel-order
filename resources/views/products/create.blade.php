141fcf4fc4bf74cbf65fd82a95c16707bdba23d2
voldemarasp <voldemarasp@gmail.com>
2018-03-15 10:22:20 +0200

    autocomplete create product

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -86,44 +86,79 @@
                     </div>
                 </div>
 
+            </div>
+            <div class="col-lg-6 col-md-12">
+                <div class="form-group">
+                    <label class="col control-label">Release date</label>
+                    <div class="col inputGroupContainer">
+                        <div class="input-group">
+                            <input  name="release_date" placeholder="Release date" class="form-control"  type="date" value="{{ old('release_date') }}">
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col control-label">PEGI</label>
+                    <div class="col inputGroupContainer">
+                        <div class="input-group">
+                            <input  name="pegi" placeholder="PEGI" class="form-control"  type="number" value="{{ old('video') }}">
+                        </div>
+                    </div>
+                </div>
 
+                <div class="form-group">
+                    <label class="col control-label">Video URL</label>
+                    <div class="col inputGroupContainer">
+                        <div class="input-group">
+                            <input  name="video" placeholder="Video URL" class="form-control"  type="text" value="{{ old('video') }}">
+                        </div>
+                    </div>
+                </div>
 
-            <label for="stock_amount">Stock:</label>
-            <br>
-            <input type="number" name="stock_amount" class="form-control" placeholder="Available stock" value="{{ old('stock_amount') }}">
-            <br>
-            <label for="price_amount">Price:</label>
-            <br>
-            <input type="text" step="0.01" name="price_amount" class="form-control" placeholder="Price" value="{{ old('price_amount') }}">
-            <br>
+                <div class="form-group">
+                    <label class="col control-label">Images</label>
+                    <div class="col inputGroupContainer">
+                        <div class="input-group">
+                            <input type="file" class="form-control-file" name="image" id="image">
+                        </div>
+                    </div>
+                </div>
 
+                <div class="form-group">
+                    <label class="col control-label">Price</label>
+                    <div class="col inputGroupContainer">
+                        <div class="input-group">
+                            <input  name="price_amount" placeholder="Price" class="form-control"  type="text" value="{{ old('price_amount') }}">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col control-label">Stock</label>
+                    <div class="col inputGroupContainer">
+                        <div class="input-group">
+                            <input  name="stock_amount" placeholder="Stock" class="form-control"  type="text" value="{{ old('stock_amount') }}">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="col control-label">Synapsis</label>
+                    <div class="col inputGroupContainer">
+                        <div class="input-group">
+                            <textarea class="form-control" name="description" placeholder="Synapsis" rows="2">{{ old('description') }}</textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="col-lg-6 col-md-12">
-
-
-            <br>
-            <label for="description">Description:</label>
-            <br>
-            <textarea type="text" name="description" id="description" class="form-control"
-            placeholder="Product description">{{ old('description') }}</textarea>
-            <br>
-            <label for="release_date">Release Date:</label>
-            <br>
-            <input type="date" name="release_date" class="form-control" value="{{ old('release_date') }}">
-            <br>
-            <label for="video">Video</label>
-            <br>
-            <input type="text" name="video" class="form-control" value="{{ old('video') }}">
-            <br>
-            <label for="pegi">Pegi</label>
-            <br>
-            <input type="number" name="pegi" class="form-control" value="{{ old('pegi') }}">
-            <br>
-            <input type="file" name="image" id="image" >
-            <br>
-            <button type="submit" class="btn btn-secondary">Submit</button>
+        <div class="row">
+        <div class="col-12 form-group">
+            <div class="col">
+                <button type="submit" class="btn btn-danger btn-block" >Create</button>
+            </div>
         </div>
     </div>
-</div>
+    </div>
 </form>
+</div>
 @endsection
