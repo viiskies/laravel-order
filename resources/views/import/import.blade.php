7837d42591d3cfa3bb68eacd16df737abb485277
voldemarasp <voldemarasp@gmail.com>
2018-03-20 14:03:50 +0200

    importai produkto

--- resources/views/import/import.blade.php
+++ resources/views/import/import.blade.php
@@ -1,37 +1,50 @@
-@extends('layouts.app')
+@extends('layouts.page')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6 mx-auto">
-                @if($errors->has('file'))
-                    <small class="alert alert-danger">
-                        {{ $errors->first('file') }}
-                    </small>
-                @endif
-                @if (Session::has('error'))
-                    <small class="alert alert-danger">
-                        {{ Session::get('error') }}
-                    </small>
-                @endif
-                @if (Session::has('success'))
-                    <small class="alert alert-success">
-                        {{ Session::get('success') }}
-                    </small>
-                @endif
-            </div>
+<div class="col-10">
+    <div class="row">
+        <div class="col-12 mt-5 mb-5 text-center">
+            <h2>Import products</h2>
         </div>
-        <div class="row">
-            <div class="col-sm-6 mx-auto mt-4">
-                <form action="{{ route('products.import') }}" method="post" enctype="multipart/form-data">
-                    {{ csrf_field() }}
-                    <div class="form-group">
-                        <label>Import excel file</label>
-                        <input type="file" class="form-control-file" name="file">
-                        <input type="submit" value="Import">
-                    </div>
-                </form>
-            </div>
+        <div class="col-sm-6 mx-auto">
+            @if($errors->has('file'))
+            <small class="alert alert-danger">
+                {{ $errors->first('file') }}
+            </small>
+            @endif
+            @if (Session::has('error'))
+            <small class="alert alert-danger">
+                {{ Session::get('error') }}
+            </small>
+            @endif
+            @if (Session::has('success'))
+            <small class="alert alert-success">
+                {{ Session::get('success') }}
+            </small>
+            @endif
         </div>
     </div>
+    <div class="row">
+        <div class="col-sm-6 mx-auto mt-4">
+            <form action="{{ route('products.import') }}" method="post" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <div class="input-group mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">Import file</span>
+                    </div>
+                    <div class="custom-file">
+                        <input type="file" name="file" class="custom-file-input" id="inputGroupFile01">
+                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                    </div>
+                </div>
+                <div class="d-flex justify-content-center">
+                <button class="btn btn-outline-dark" type="submit" name="submit" value="Import">Import</button>
+            </div>
+            </div>
+        </form>
+    </div>
+</div>
+</div>
+</div>
 @endsection
--- resources/views/import/log.blade.php
+++ resources/views/import/log.blade.php
@@ -1,56 +1,68 @@
-@extends('layouts.app')
+@extends('layouts.main')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+<div class="col-10">
+    <div class="row">
+        <div class="col-12 mt-5 mb-5 text-center">
+            <h2>Import products log</h2>
+        </div>
+        <div class="col-12">
             <form method="post" action="{{route('products.import.filter')}}">
                 @csrf
-                <select name="status">
+                <div class="input-group">
+                  <select class="custom-select" id="inputGroupSelect04">
                     <option value="all">All</option>
                     <option value="{{ \App\ImportItem::WAITING }}">Waiting</option>
                     <option value="{{ \App\ImportItem::IN_PROGRESS }}">In progress</option>
                     <option value="{{ \App\ImportItem::DONE }}">Success</option>
                     <option value="{{ \App\ImportItem::FAIL }}">Fail</option>
                 </select>
-                <button type="submit">Filter</button>
-            </form>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Line</th>
-                        <th>Id</th>
-                        <th>Name</th>
-                        <th>Status</th>
-                        <th>Error</th>
-                        <th>Upload time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($all as $line => $one_line)
-                        <tr>
-                            <td>{{($all->currentpage()-1) * $all->perpage() + $line + 1}}</td>
-                            <td>{{$one_line->product_id}}</td>
-                                <td>
-                                    @if($one_line->product_id !== null)
-                                        <a href="{{ route('products.show', $one_line->product_id) }}">{{$one_line->product_name}}</a>
-                                    @endif
-                                </td>
-                            <td>
-                                @if ($one_line->status == \App\ImportItem::FAIL)
-                                    <span class="text-danger">{{$one_line->import_status}}</span>
-                                @else
-                                    {{$one_line->import_status}}
-                                @endif
-                            </td>
-                            <td>{{$one_line->error_message}}</td>
-                            <td>{{$one_line->updated_at}}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-        <div class="row ">
-            <div class="col mx-auto">{{$all->links()}}</div>
-        </div>
+                <div class="input-group-append">
+                    <button class="btn btn-outline-dark" type="submit">Filter</button>
+                </div>
+            </div>
+        </form>
     </div>
+    <div class="col-12">
+        <table>
+            <thead>
+                <tr>
+                    <th>Line</th>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Status</th>
+                    <th>Error</th>
+                    <th>Upload time</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($all as $line => $one_line)
+                <tr>
+                    <td>{{($all->currentpage()-1) * $all->perpage() + $line + 1}}</td>
+                    <td>{{$one_line->product_id}}</td>
+                    <td>
+                        @if($one_line->product_id !== null)
+                        <a href="{{ route('products.show', $one_line->product_id) }}">{{$one_line->product_name}}</a>
+                        @endif
+                    </td>
+                    <td>
+                        @if ($one_line->status == \App\ImportItem::FAIL)
+                        <span class="text-danger">{{$one_line->import_status}}</span>
+                        @else
+                        {{$one_line->import_status}}
+                        @endif
+                    </td>
+                    <td>{{$one_line->error_message}}</td>
+                    <td>{{$one_line->updated_at}}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="col-12 d-flex justify-content-center mt-3">
+    {{$all->links()}}
+</div>
+</div>
+</div>
 @endsection
