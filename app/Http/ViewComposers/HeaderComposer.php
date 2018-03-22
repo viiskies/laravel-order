9d8f2e6c304913fa009fb14f7df9e8a663b1e8bc
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2018-03-22 14:23:45 +0200

    country su view composer fixed

--- app/Http/ViewComposers/HeaderComposer.php
+++ app/Http/ViewComposers/HeaderComposer.php
@@ -11,21 +11,22 @@ class HeaderComposer {
     public function compose(View $view) {
 
         $user = Auth::user();
+
         if(empty($user->country)){
+
             $default_country = Country::where('default', 1)->first();
+
             if($default_country == null) {
                 $email = 'paulius@gamestar.lt';
                 $phone = '869889141';
-                $view->with(['email' => $email, 'phone' => $phone]);
            } else {
                 $email = $default_country->email;
                 $phone = $default_country->phone;
-                $view->with(['email' => $email, 'phone' => $phone]);
             }
         }else {
             $email = $user->country->email;
             $phone = $user->country->phone;
-            $view->with(['email' => $email, 'phone' => $phone]);
         }
+        $view->with(['email' => $email, 'phone' => $phone]);
     }
 }
