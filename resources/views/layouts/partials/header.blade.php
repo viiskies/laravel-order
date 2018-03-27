ed5f6f6e425e0c79c71fc5396b1ad0b6ac625aaf
voldemarasp <voldemarasp@gmail.com>
2018-03-27 10:02:21 +0300

    logout ir username top bare

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,59 +1,63 @@
 @inject('cartService', "App\Services\CartService")
 <!-- Top Bar -->
-		<div class="row">
-			<div class="top-bar">
-				<ul>
-					<li><i class="fa fa-phone-volume"></i>  <a href="#">{{$phone}}</a></li>
-					<li><i class="fa fa-envelope"></i>  <a href="#">{{$email}}</a></li>
-				</ul>
-			</div>
-		</div>
-			<!-- Header -->
-			<div class="row">
-				<div class="logo">
-					<a href="{{ route('home') }}"><img src="{{asset('images/logo2.png')}}"></a>
-				</div>
-				<div class="cart-menu-mobile">
+<div class="row">
+ <div class="top-bar">
+    <ul>
+        <li><i class="fa fa-angle-right"></i>  <a href="{{ route('logout') }}">Logout</a></li>
+        <li><i class="fa fa-user"></i>  {{{ isset(Auth::user()->name) ? Auth::user()->name : Auth::user()->email }}}</li>
+    </ul>
+    <ul>
+       <li><i class="fa fa-phone"></i>  <a href="#">{{$phone}}</a></li>
+       <li><i class="fa fa-envelope"></i>  <a href="#">{{$email}}</a></li>
+   </ul>
+</div>
+</div>
+<!-- Header -->
+<div class="row">
+    <div class="logo">
+       <a href="{{ route('home') }}"><img src="{{asset('images/logo2.png')}}"></a>
+   </div>
+   <div class="cart-menu-mobile">
 
-					<span class="cart-menu-icon-mobile">
-						<a href="{{ route('order.index') }}"><i class="fa fa-cart-arrow-down"></i></a>
-					</span>
+       <span class="cart-menu-icon-mobile">
+          <a href="{{ route('order.index') }}"><i class="fa fa-cart-arrow-down"></i></a>
+      </span>
+  </div>
+  <div class="nav d-flex justify-content-end ">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link btn {{ (\Request::route()->getName() == 'home') ? 'btn-outline-danger' : 'btn-danger' }}" href="{{ route('home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-danger" href="#">Order</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn btn-danger" href="#">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link btn {{ (\Request::route()->getName() == 'pages.contacts') ? 'btn-outline-danger' : 'btn-danger' }}" href="{{ route('pages.contacts') }}">Contact</a>
+            </li>
+        </ul>
     </div>
-    <div class="nav d-flex justify-content-end ">
-        <nav class="navbar navbar-expand-lg navbar-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link btn {{ (\Request::route()->getName() == 'home') ? 'btn-outline-danger' : 'btn-danger' }}" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-danger" href="#">Order</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn btn-danger" href="#">Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link btn {{ (\Request::route()->getName() == 'pages.contacts') ? 'btn-outline-danger' : 'btn-danger' }}" href="{{ route('pages.contacts') }}">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
-    <div class="cart-menu">
-        <a href="{{ route('order.index') }}">
-            <span class="cart-menu-inside">
-                <span class="cart-menu-icon">
-                    <i class="fa fa-cart-arrow-down"></i>
-                </span>
-                <span class="cart-menu-price totalQuantityTop">Items: {{ $cartService->getUserOrderTotalQuantity()}}</span>
-                <span class="cart-menu-price totalPriceTop">  € {{ $cartService->getUserOrderTotalPrice() }}</span>
+</nav>
+</div>
+<div class="cart-menu">
+    <a href="{{ route('order.index') }}">
+        <span class="cart-menu-inside">
+            <span class="cart-menu-icon">
+                <i class="fa fa-cart-arrow-down"></i>
             </span>
-        </a>
-    </div>
+            <span class="cart-menu-price totalQuantityTop">Items: {{ $cartService->getUserOrderTotalQuantity()}}</span>
+            <span class="cart-menu-price totalPriceTop">  € {{ $cartService->getUserOrderTotalPrice() }}</span>
+        </span>
+    </a>
+</div>
 </div>
 <!-- Slider -->
 <div id="search" class="row justify-content-center">
@@ -79,11 +83,11 @@
     <div class="col-12 d-flex justify-content-center pt-3 pb-3 search-bar-back">
         <form class="form-inline justify-content-center" action="{{ route('products.search') }}" method="GET">
             @if (isset($query))
-                <input name="query" id="productsSearch" class="form-control mr-sm-2 search-inputas" type="search"
-                       placeholder="Search" aria-label="Search" value="{{ old('name', $query) }}">
+            <input name="query" id="productsSearch" class="form-control mr-sm-2 search-inputas" type="search"
+            placeholder="Search" aria-label="Search" value="{{ old('name', $query) }}">
             @else
-                <input name="query" id="productsSearch" class="form-control mr-sm-2 search-inputas" type="search"
-                       placeholder="Search" aria-label="Search" value="{{ old('name') }}">
+            <input name="query" id="productsSearch" class="form-control mr-sm-2 search-inputas" type="search"
+            placeholder="Search" aria-label="Search" value="{{ old('name') }}">
             @endif
             <button class="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
         </form>
