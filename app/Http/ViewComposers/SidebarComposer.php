79adfe74a0924beb2e925a7c8d4a0bcd7485348a
edvinasdau <30698620+edvinasdau@users.noreply.github.com>
2018-03-27 13:55:14 +0300

    new arrivals isrinkimas is db ir atvaizdavimas

--- app/Http/ViewComposers/SidebarComposer.php
+++ app/Http/ViewComposers/SidebarComposer.php
@@ -20,7 +20,21 @@ class SidebarComposer
     public function compose(View $view)
     {
         $cats = Category::all();
-        $products_latest = Product::orderBy('id', 'desc')->take(8)->get();
+        $products = Product::all();
+        $products_latest = [];
+
+        foreach ($products as $product) {
+            $stock = $product->stock()->orderBy('id', 'desc')->take(2)->get();
+
+            if (count($stock) == 1) {
+                $products_latest[] = $product;
+            }
+            if (count($stock) > 1) {
+                if ($stock[0]->amount > $stock[1]->amount) {
+                    $products_latest[] = $product;
+                }
+            }
+        }
         $mostPopularProducts = $this->productService->getMostPopular();
         $view->with(['cats' => $cats, 'products_latest' => $products_latest, 'mpp' => $mostPopularProducts]);
     }
